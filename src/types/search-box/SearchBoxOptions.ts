6589e627955bc210dfd4bf9920ba5cf67d718446
aivaras <aivaras@lupasearch.com>
2024-06-07 15:10:54 +0300

    Support for close search box panel button

--- src/types/search-box/SearchBoxOptions.ts
+++ src/types/search-box/SearchBoxOptions.ts
@@ -26,6 +26,7 @@ export type SearchBoxOptionLabels = {
   defaultFacetLabel?: string
   close?: string
   searchInputAriaLabel?: string
+  closePanel?: string;
 }
 
 export type SearchBoxOptionLinks = {
