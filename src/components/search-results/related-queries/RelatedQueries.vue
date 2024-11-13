2c32114308369afb92b54b2453004cb4539a7723
aivaras <aivaras@lupasearch.com>
2024-11-13 13:19:19 +0200

    Support for filterable related queries

--- src/components/search-results/related-queries/RelatedQueries.vue
+++ src/components/search-results/related-queries/RelatedQueries.vue
@@ -6,6 +6,9 @@ import { computed } from 'vue'
 import { extractRelatedSource } from '@/utils/relatedSource.utils'
 import RelatedQueryPanel from './RelatedQueryPanel.vue'
 import { useParamsStore } from '@/stores/params'
+import { useOptionsStore } from '@/stores/options'
+import { toggleTermFilter } from '@/utils/filter.toggle.utils'
+import { FilterGroupItemTypeTerms } from '@getlupa/client-sdk/Types'
 
 const props = defineProps<{
   options?: RelatedQueryOptions
@@ -13,13 +16,18 @@ const props = defineProps<{
 
 const searchResultStore = useSearchResultStore()
 const paramsStore = useParamsStore()
+const optionsStore = useOptionsStore()
+
 const { searchResult } = storeToRefs(searchResultStore)
+
+const currentSearchText = computed(() => searchResult.value?.searchText ?? '')
 
 const relatedQueries = computed(() => {
   if (!props.options || !searchResult.value) {
     return []
   }
-  return extractRelatedSource(props.options.source, searchResult.value)
+  const queries = extractRelatedSource(props.options.source, searchResult.value)
+  return queries
 })
 
 const hasEnoughRelatedQueries = computed(() => {
@@ -29,6 +37,41 @@ const hasEnoughRelatedQueries = computed(() => {
 const goToResults = ({ searchText }: { searchText: string }) => {
   paramsStore.goToResults({ searchText })
 }
+
+const handleRelatedQueryClick = (query: string) => {
+  if (props.options.source?.mode === 'filter') {
+    handleFilter(query)
+  } else {
+    goToResults({ searchText: query })
+  }
+}
+
+const handleFilter = (query: string) => {
+  const facet = searchResult.value?.facets?.find(
+    (facet) => facet.key === props.options?.source?.key
+  )
+  if (!facet || facet.type !== 'terms') {
+    return []
+  }
+  toggleTermFilter(
+    paramsStore.appendParams as any,
+    { type: 'terms', key: facet.key, value: query },
+    optionsStore.getQueryParamName,
+    {}
+  )
+}
+
+const getSelectedFilterClass = (query: string) => {
+  if (props.options.source?.mode !== 'filter') {
+    return ''
+  }
+  return Array.isArray(searchResult.value?.filters?.[props.options.source.key]) &&
+    (searchResult.value?.filters?.[props.options.source.key] as FilterGroupItemTypeTerms)?.includes(
+      query
+    )
+    ? 'lupa-selected-related-query-filter'
+    : ''
+}
 </script>
 <template>
   <div v-if="options && hasEnoughRelatedQueries" class="lupa-related-queries">
@@ -36,8 +79,12 @@ const goToResults = ({ searchText }: { searchText: string }) => {
       {{ options?.labels?.title }}
     </h3>
     <ul>
-      <li v-for="query of relatedQueries" :key="query">
-        <a @click="goToResults({ searchText: query })">
+      <li
+        v-for="query of relatedQueries"
+        :key="query + currentSearchText"
+        :class="getSelectedFilterClass(query)"
+      >
+        <a @click="handleRelatedQueryClick(query)">
           <RelatedQueryPanel :options="options" :query="query" />
         </a>
       </li>
